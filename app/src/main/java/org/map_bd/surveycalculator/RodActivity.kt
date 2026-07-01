d258e690f8964dace4fc4ac3db03b79a7ad8a5f7
Atikur Rahman <55056373+arahmandc@users.noreply.github.com>
2026-07-02 01:48:37 +0600

    update

--- app/src/main/java/org/map_bd/surveycalculator/RodActivity.kt
+++ app/src/main/java/org/map_bd/surveycalculator/RodActivity.kt
@@ -1,11 +1,14 @@
 package org.map_bd.surveycalculator
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import org.map_bd.surveycalculator.databinding.ActivityGenarelBinding
 import org.map_bd.surveycalculator.databinding.ActivityRodBinding
 
 
@@ -23,36 +26,92 @@ class RodActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
 
+        var mm8 = findViewById<EditText>(R.id.mm8Id)
+        var mm10 = findViewById<EditText>(R.id.mm10Id)
+        var mm12 = findViewById<EditText>(R.id.mm12Id)
+        var mm16 = findViewById<EditText>(R.id.mm16Id)
+        var mm20 = findViewById<EditText>(R.id.mm20Id)
+        var mm25= findViewById<EditText>(R.id.mm25Id)
+
+
+        var mmp8 = findViewById<EditText>(R.id.mmp8Id)
+        var mmp10 = findViewById<EditText>(R.id.mmp10Id)
+        var mmp12 = findViewById<EditText>(R.id.mmp12Id)
+        var mmp16 = findViewById<EditText>(R.id.mmp16Id)
+        var mmp20 = findViewById<EditText>(R.id.mmp20Id)
+        var mmp25= findViewById<EditText>(R.id.mmp25Id)
+
+
+        var price = findViewById<EditText>(R.id.priceId)
+
+        var kg = findViewById<TextView>(R.id.kgId)
+        var priz = findViewById<TextView>(R.id.prizeId)
+
+
+        var reset =findViewById<Button>(R.id.resetId)
+
+
+        var result = findViewById<LinearLayout>(R.id.resultId)
+
+
+        binding.calculateId.setOnClickListener {
+
+
+            var mm8 = mm8.text.toString().toDoubleOrNull()
+            var mm10 = mm10.text.toString().toDoubleOrNull()
+            var mm12 = mm12.text.toString().toDoubleOrNull()
+            var mm16 = mm16.text.toString().toDoubleOrNull()
+            var mm20 = mm20.text.toString().toDoubleOrNull()
+            var mm25 = mm25.text.toString().toDoubleOrNull()
+
+            var mmp8 = mmp8.text.toString().toDoubleOrNull()
+            var mmp10 = mmp10.text.toString().toDoubleOrNull()
+            var mmp12 = mmp12.text.toString().toDoubleOrNull()
+            var mmp16 = mmp16.text.toString().toDoubleOrNull()
+            var mmp20 = mmp20.text.toString().toDoubleOrNull()
+            var mmp25 = mmp25.text.toString().toDoubleOrNull()
+
+
+
+
+
+
+            if (mm8 != null || mm10 != null || mm12 != null || mm16 != null || mm20 != null || mm25 != null ||
+                mmp8 != null || mmp10 != null || mmp12 != null || mmp16 != null || mmp20 != null || mmp25 != null) {
+
+
+                while (mmp8 != null) {
+                    mmp8 = mm8!! * 0.120
+                }
+
+
+
+
+            } else {
+                Toast.makeText(this, "Please add any number", Toast.LENGTH_SHORT).show()
+            }
+            closeKeyBoard()
+
+            reset.visibility = View.VISIBLE
+            result.visibility = View.VISIBLE
+
+
+        }
+
+
 
 
     }
 
-//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-//        menuInflater.inflate(R.menu.menu, menu)
-//        return true
-//    }
+    private fun closeKeyBoard() {
+        val view = this.currentFocus
+        if (view != null) {
+            val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+    }
 
-//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-//        when(item.itemId){
-//            android.R.id.home ->{
-//                finish()
-//                return true
-//            }
-//            R.id.aboutId -> {
-//                val about = Intent(this,AboutActivity::class.java);
-//                startActivity(about)
-//            }
-//            R.id.basicCal ->{
-//                val calculator = Intent(this,BasicalculatorActivity::class.java);
-//                startActivity(calculator)
-//            }
-//            R.id.compassId ->{
-//                val compass = Intent(this,CompassActivity::class.java);
-//                startActivity(compass)
-//            }
-//        }
-//        return true
-//    }
+
 
 
 }
